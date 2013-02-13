0004269bccac33503b53e8e61f5cf2cf679af3c7
James Vanneman <jvanneman@conciergelive.com>
2013-02-13 12:15:03 -0500

    initial commit

---
+++ neco/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neco::Application.config.secret_token = 'e7a45de8b739c85f48a20bbf7ce3a2e2c3a81faa3754117b7742889c80ad58f8332c661182f2fed59c1243d465f0ad530b45480c5223f25e4eb0971900814964'
